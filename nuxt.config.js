5f841e96b17c6965ad9d99d41b29bf19ad7e1964
dygao <dygaomail@gmail.com>
2022-04-15 08:04:02 -0300

    🔧 Configuração para Atomic Design

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,12 @@ export default {
   plugins: ['@/plugins/fontawesome'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    {
+      path: '@/components',
+      pathPrefix: false,
+    },
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
